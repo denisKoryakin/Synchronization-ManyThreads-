a63de07cc9ccf35cb3744afadca09e60ce77b266
Denis Koryakin <singler1991@gmail.com>
2023-02-23 18:03:38 +0500

    refactoring code with synchronization save approach

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,43 +13,58 @@ public class Main {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        Callable<Integer> myCallable = () -> {
-            String way = generateRoute("RLRFR", wayLength);
-            char[] arr = way.toCharArray();
-            int result = 0;
-            for (char c : arr) {
-                if (c == 'R') {
-                    result++;
+//        создаем поток для мониторинга состояния мапы
+        Thread monitor = new Thread(() -> {
+            int iterationCount = 0;
+            while (!Thread.interrupted()) {
+                synchronized (sizeToFreq) {
+                    try {
+                        sizeToFreq.wait();
+                    } catch (InterruptedException e) {
+                        return;
+                    }
+                    System.out.println("На этапе " + iterationCount);
+                    printEndResult(sizeToFreq);
+                    iterationCount++;
                 }
             }
-            return (result * 100) / arr.length;
-        };
+        });
+        monitor.start();
 
-//      Создали список из 1000 задач
-        List<Callable<Integer>> callableList = new ArrayList<>();
+//        создаем коллекцию из 1000 потоков для поиска 1000 маршрутов
+        List<Thread> tasksTreads = new ArrayList<>();
         for (int i = 0; i < threads; i++) {
-            callableList.add(myCallable);
+            tasksTreads.add(new Thread(() -> {
+                String way = generateRoute("RLRFR", wayLength);
+                char[] arr = way.toCharArray();
+                int result = 0;
+                for (char c : arr) {
+                    if (c == 'R') {
+                        result++;
+                    }
+                }
+                result = (result * 100) / arr.length;
+//                записываем значение в мапу
+                synchronized (sizeToFreq) {
+                    if (sizeToFreq.containsKey(result)) {
+                        sizeToFreq.put(result, sizeToFreq.get(result) + 1);
+                    } else {
+                        sizeToFreq.put(result, 1);
+                    }
+                    sizeToFreq.notify();
+                }
+            }));
+//            Заставляем заснуть поток для отработки мониторинга мапы другим потоком
+            tasksTreads.get(tasksTreads.size() - 1).start();
+            Thread.sleep(1);
         }
-//      Создали лист из 1000 будущих решений
-        List<Future<Integer>> futureList;
 
-//      создали пул из 1000 потоков
-        final ExecutorService threadPool = Executors.newFixedThreadPool(threads);
-
-//      заполняем лист futureList решениями всех задач из списка callableList
-        futureList = threadPool.invokeAll(callableList);
-
-        if (threadPool.isTerminated()) threadPool.shutdown();
-
-//      заполняем мапу
-        for (Future<Integer> integerFuture : futureList) {
-            if (sizeToFreq.containsKey(integerFuture.get())) {
-                sizeToFreq.replace(integerFuture.get(), sizeToFreq.get(integerFuture.get()) + 1);
-            } else {
-                sizeToFreq.put(integerFuture.get(), 1);
-            }
+//        дожидаемся завершения работы всех потоков по решению задач
+        for (Thread thread : tasksTreads) {
+            thread.join();
         }
-        printResult(sizeToFreq);
+        monitor.interrupt();
+        printOtherResults(sizeToFreq);
     }
 
     public static String generateRoute(String letters, int length) {
@@ -61,18 +76,25 @@ public class Main {
         return route.toString();
     }
 
-    public static void printResult(Map<Integer, Integer> map) {
-        int maxValue = Collections.max(map.values());
+    public static void printEndResult(Map<Integer, Integer> map) {
         for (Map.Entry<Integer, Integer> entry :
                 map.entrySet()) {
-            if (entry.getValue() == maxValue) {
-                System.out.println("Самое частое количество повторений " + entry.getKey() + " встречается " + maxValue + " раз");
+            if (entry.getValue().equals(Collections.max(map.values()))) {
+                System.out.println("Самое частое количество повторений " + entry.getKey() + " встречается " + Collections.max(map.values()) + " раз");
+                break;
             }
         }
+    }
+
+    public static void printOtherResults(Map<Integer, Integer> map) throws InterruptedException {
+//        ввремя необходимое для отрабатывания параллельных потоков
+        Thread.sleep(5);
         System.out.println("Другие размеры: ");
         for (Map.Entry<Integer, Integer> entry :
                 map.entrySet()) {
-            System.out.println("(" + entry + " раз)");
+            if (!entry.getValue().equals(Collections.max(map.values()))) {
+                System.out.println("(" + entry + " раз)");
+            }
         }
     }
 }
